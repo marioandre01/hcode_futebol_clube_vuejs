72b05ab32e46b313d380d28e31a49eac47fe93cc
Mário André <mario_andre.l.a@hotmail.com>
2021-01-05 12:33:45 -0300

    created component Section and SectionBanner

---
+++ src/components/HcodeSection.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <HcodeSectionBanner />
+    </section>
+</template>
+
+<script>
+import HcodeSectionBanner from './HcodeSectionBanner'
+
+export default {
+    components: {
+        HcodeSectionBanner
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
