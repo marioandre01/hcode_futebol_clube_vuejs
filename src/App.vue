229a3574511eb02843517c026b6d991ac49bbfed
Mário André <mario_andre.l.a@hotmail.com>
2021-01-05 12:30:35 -0300

    created component Footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HcodeHeader />
+    <HcodeFooter />
   </div>
 </template>
 
 <script>
 import HcodeHeader from './components/HcodeHeader'
+import HcodeFooter from './components/HcodeFooter'
 
 export default {
   name: 'App',
   components: {
-    HcodeHeader  
+    HcodeHeader,
+    HcodeFooter, 
   }
 }
 </script>
