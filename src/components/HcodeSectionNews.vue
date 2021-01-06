9bfdbff3bf36837eb92ab9c5d6a590df676e3010
Mário André <mario_andre.l.a@hotmail.com>
2021-01-06 12:04:40 -0300

    applying Vuex getters with mapGetters in the SectionNews component

--- src/components/HcodeSectionNews.vue
+++ src/components/HcodeSectionNews.vue
@@ -22,43 +22,22 @@
 
 <script>
 import HcodeSectionNewsIndividual from './HcodeSectionNewsIndividual'
-
+import { mapGetters } from 'vuex'
+ 
 export default {
     components: {
         HcodeSectionNewsIndividual
     },
     data() {
         return {
-            news: []
-        }
-        
+            
+        }      
     },
-    created() {
-        this.news = [{
-            id: 1,
-            title:'Começaram os treinos da nova temporada',
-            content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum ullam necessitatibus tempore repudiandae? Autem ratione architecto omnis possimus, explicabo officia sequi aspernatur perspiciatis corrupti harum distinctio ex eum. Itaque, voluptas. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores quasi, natus labore repudiandae corporis sint mollitia aliquam minima quod sit voluptatum autem nemo et facilis provident optio expedita sapiente maxime.',
-            date:'2021-01-02',
-            img: 'news1.jpg',
-            amgInfo: 'Notícia 1'
-        },{
-            id: 2,
-            title:'Jogo de quarta-feira termina empatado',
-            content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum ullam necessitatibus tempore repudiandae? Autem ratione architecto omnis possimus, explicabo officia sequi aspernatur perspiciatis corrupti harum distinctio ex eum. Itaque, voluptas. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores quasi, natus labore repudiandae corporis sint mollitia aliquam minima quod sit voluptatum autem nemo et facilis provident optio expedita sapiente maxime.',
-            date:'2021-05-01',
-            img: 'news2.jpg',
-            amgInfo: 'Notícia 2'
-        },{
-            id: 3,
-            title:'Novo estádio será inaugurado na semana que vem',
-            content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum ullam necessitatibus tempore repudiandae? Autem ratione architecto omnis possimus, explicabo officia sequi aspernatur perspiciatis corrupti harum distinctio ex eum. Itaque, voluptas. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores quasi, natus labore repudiandae corporis sint mollitia aliquam minima quod sit voluptatum autem nemo et facilis provident optio expedita sapiente maxime.',
-            date:'2021-03-02',
-            img: 'news3.jpg',
-            amgInfo: 'Notícia 3'
-        }
-        
-        ]
-    }
+    computed: {
+        ...mapGetters({
+            news: 'getNews'
+        })
+    }   
 }
 </script>
 
