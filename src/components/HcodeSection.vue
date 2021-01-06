1648c2658b12d37f8b614605598413bdf182bb86
Mário André <mario_andre.l.a@hotmail.com>
2021-01-05 23:14:19 -0300

    created Input component and used v-model

--- src/components/HcodeSection.vue
+++ src/components/HcodeSection.vue
@@ -7,17 +7,37 @@
         </div>
         <HcodeSectionBanner />
         <HcodeSectionNews />
+
+        <div class="container">
+            <div class="row my-club mt-5">
+                 <div class="col-6">
+                     <h2>Seu clube é: {{ myClub }}</h2>
+                 </div>
+
+                 <div class="col-6">
+                     <HcodeInput v-model="myClub" />
+                 </div>
+            </div>
+        </div>
+
     </section>
 </template>
 
 <script>
 import HcodeSectionBanner from './HcodeSectionBanner'
 import HcodeSectionNews from './HcodeSectionNews'
+import HcodeInput from './HcodeInput'
 
 export default {
     components: {
         HcodeSectionBanner,
-        HcodeSectionNews
+        HcodeSectionNews,
+        HcodeInput
+    },
+    data() {
+        return {
+            myClub: 'Hcode Treinamentos'
+        }
     },
     props: {
         championship: String
