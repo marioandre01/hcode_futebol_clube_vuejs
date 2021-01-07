989a7e0f14a72c20b52e7e64cd70717f955d8d70
Mário André <mario_andre.l.a@hotmail.com>
2021-01-07 00:11:11 -0300

    testing Navigation Guards - Global

--- src/main.js
+++ src/main.js
@@ -10,6 +10,17 @@ Vue.use(Vue2Filters)
 
 Vue.config.productionTip = false
 
+/* Global Guards -> realiza uma verificação em todas as rotas da aplicação 
+router.beforeEach((to, from, next) => {
+  console.log(to, from);
+
+  next();
+});
+
+router.afterEach(() => {
+  console.log('Renderização ok');
+}); */
+
 new Vue({
   render: h => h(App),
   store,
